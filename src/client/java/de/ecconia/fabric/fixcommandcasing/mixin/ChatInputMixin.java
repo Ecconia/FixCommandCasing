9bf08f913c9f969429251cc4ff740d081b7a6068
Ecconia <public-commit@ecconia.com>
2026-04-17 18:18:21 +0200

    Update fabric & MC to 26.1
    Use the 26.1 fabric example mod to update gradle and associated files.
    Switch acesswidener to 'official' namespace - due to that being required now (not documented).
    Ofc, fix code for MC changes.

--- src/client/java/de/ecconia/fabric/fixcommandcasing/mixin/ChatInputMixin.java
+++ src/client/java/de/ecconia/fabric/fixcommandcasing/mixin/ChatInputMixin.java
@@ -1,7 +1,7 @@
 package de.ecconia.fabric.fixcommandcasing.mixin;
 
-import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,12 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ChatScreen.class)
 public class ChatInputMixin
 {
-	@Shadow protected TextFieldWidget chatField;
+	@Shadow protected EditBox input;
 	
-	@Inject(method = "onChatFieldUpdate", at = @At("HEAD"))
+	@Inject(method = "onEdited", at = @At("HEAD"))
 	private void onInputChange(String chatText, CallbackInfo ci)
 	{
-		String text = this.chatField.getText();
+		String text = this.input.getValue();
 		if(text.isEmpty() || text.charAt(0) != '/')
 		{
 			return;
@@ -27,6 +27,6 @@ public class ChatInputMixin
 			indexOfSpace = text.length();
 		}
 		//Overwrite the command with lowercase letters. Fully disallow any uppercase letter:
-		this.chatField.text = '/' + text.substring(1, indexOfSpace).toLowerCase() + text.substring(indexOfSpace);
+		this.input.value = '/' + text.substring(1, indexOfSpace).toLowerCase() + text.substring(indexOfSpace);
 	}
 }
